28ca158d2d578d1d5b605793b4836caa8c89fe70
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-02-02 21:48:47 -0500

    tasklister-challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.querySelector("#create-task-form").addEventListener("submit", (e) =>{
+      e.preventDefault();
+      createTask(e.target.create_task.value);
+    })
 });
+
+function createTask(create_task) {
+  let p = document.createElement("p");
+  p.textContent = create_task;
+  document.querySelector('#list').appendChild(p);}
+
+  
